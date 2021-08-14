ea077b0b480b5a0ac7eaee189934d71d01097965
Rahil Kapoor <rahilkapoor2000@gmail.com>
2021-08-14 14:16:22 +0530

    Built Movie Details Screen

--- lib/screens/movie_detail.dart
+++ lib/screens/movie_detail.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class MovieDetail extends StatefulWidget {
     const MovieDetail({ Key? key }) : super(key: key);
@@ -9,12 +12,65 @@ class MovieDetail extends StatefulWidget {
 
 class _MovieDetailState extends State<MovieDetail> {
 
+    XFile? _moviePoster;
+    final ImagePicker _picker = ImagePicker(); 
+
     TextEditingController titleController = TextEditingController();
     TextEditingController directorController = TextEditingController();
 
     @override
     Widget build(BuildContext context) {
+
         
+        void takePic(ImageSource source) async {
+            final pickedFile = await _picker.pickImage(
+                source: source
+            );
+
+            setState(() {
+                _moviePoster = pickedFile;
+            });
+        }
+
+        Widget selectPoster() {
+            return Container(
+                height: 100.0,
+                width: MediaQuery.of(context).size.width,
+                
+                padding: EdgeInsets.symmetric(
+                    horizontal: 20.0,
+                    vertical: 20.0
+                ),
+
+                child: Column(
+                    children: <Widget>[
+                        Text("Choose Movie Poster"),
+                        
+                        Container(height: 10.0,),
+                        
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                                TextButton(
+                                    onPressed: (){
+                                        takePic(ImageSource.camera);
+                                    },
+                                    child: Icon(Icons.camera),
+                                ),
+
+                                TextButton(
+                                    onPressed: (){
+                                        takePic(ImageSource.gallery);
+                                    },
+                                    child: Icon(Icons.image),
+                                ),
+                            ],
+                        )
+                    ]
+                )
+            );
+        }
+
         return Scaffold(
             appBar: AppBar(
                 title: Text("Edit Movie"),
@@ -25,6 +81,42 @@ class _MovieDetailState extends State<MovieDetail> {
 
                 child: ListView(
                     children: <Widget>[
+
+                        Center(
+                            child: Stack(
+                                children: <Widget>[
+                                    CircleAvatar(
+                                        radius: 80.0,
+                                        // backgroundImage: _moviePoster == null
+                                        //     ? AssetImage("assets/poster.jpg")
+                                        //     : FileImage(File(_moviePoster.path)),
+                                        
+                                    ),
+
+                                    Positioned(
+                                        bottom: 20.0,
+                                        right: 20.0,
+                                        child: InkWell(
+                                            onTap: (){
+                                                showModalBottomSheet(
+                                                    context: context,
+                                                    builder: ((builder) => selectPoster()),
+                                                );
+                                            },
+
+                                            child: Icon(
+                                                Icons.camera_alt,
+                                                size: 30.0,
+                                                color: Colors.amber,
+                                            ),
+
+                                        ),
+
+                                    )
+                                ],
+                            ),
+                        ),
+
                         Padding(
                             padding: EdgeInsets.only(top: 20.0, left: 10.0, right: 10.0),
                             child: TextField(
